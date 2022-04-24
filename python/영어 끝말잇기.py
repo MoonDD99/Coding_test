8c4e80e5bb509020dfb12dacd9098b6d7efec363
MoonDD99 <mjh991016@naver.com>
2022-04-24 18:27:49 +0900

    Programmers : 영어 끝말잇기

---
+++ python/영어 끝말잇기.py
@@ -0,0 +1,18 @@
+import math
+def solution(n, words):
+    #score : 100.0 / 100.0
+    answer = [0,0]
+    last_char = (words[0])[len(words[0])-1]
+    exist_word = [words[0]]
+    
+    for index in range(1,len(words)):
+        word = words[index]
+        if len(word) <= 1 or last_char != word[0] or word in exist_word :
+            answer[0] = index % n + 1
+            answer[1] = math.ceil((index+1)/n)
+            break
+        else :
+            last_char = word[len(word)-1]
+            exist_word.append(word)
+
+    return answer
