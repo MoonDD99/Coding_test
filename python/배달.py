174bca680208caded3e213f6c0cf669dbc82a6e2
MoonDD99 <mjh991016@naver.com>
2022-04-27 00:20:21 +0900

    Programmers : 배달

--- python/배달.py
+++ python/배달.py
@@ -1,24 +1,33 @@
+import heapq
+def dijkstra(dist, node_cost):
+    heap = []
+    heapq.heappush(heap,[1,0])
+
+    while heap:
+        node, cost = heapq.heappop(heap)
+        for n,c in node_cost[node]:
+            if cost + c < dist[n]:
+                dist[n] = cost + c
+                heapq.heappush(heap, [n,cost+c])
+
+    return dist
+
 def solution(N, road, K):
     answer = 0
-    times = dict() 
+    dist = [float('inf')] * (N+1)
+    dist[1] = 0
+    node_cost = [[] for _ in range(N+1)]
 
-    while len(road) > 0:
-        current_road = road.pop(0)
-        if current_road[0] == 1:
-            if times.get(current_road[1]):
-                times[current_road[1]] = min(times[current_road[1]],current_road[2])
-            else:
-                times[current_road[1]] = current_road[2]
-        else:
-            if times.get(current_road[0]):
-                times[current_road[1]] = times[current_road[0]] + current_road[2]
-            else:
-                road.append(current_road)
-                
-    return answer
+    for road_element in road:
+        node_cost[road_element[0]].append([road_element[1],road_element[2]])
+        node_cost[road_element[1]].append([road_element[0],road_element[2]])
 
-N = 5
-road = [[1,2,1],[2,3,3],[5,2,2],[1,4,2],[5,3,1],[5,4,2]]
-K = 3
+    dist = dijkstra(dist, node_cost)
+    answer = [i for i in dist if i <= K]
+    return len(answer)
+
+N = 6
+road = [[1,2,1],[1,3,2],[2,3,2],[3,4,3],[3,5,2],[3,5,3],[5,6,1]]
+K = 4
 result = solution(N,road,K)
 print(result)
