6e782a1097e52875eea2253a8eafcca6de531fac
MoonDD99 <mjh991016@naver.com>
2022-09-11 23:26:08 +0900

    Programmers : 위장

--- python/위장.py
+++ python/위장.py
@@ -1,17 +1,17 @@
+from collections import Counter
+from functools import reduce
+
 def solution(clothes):
     clothDict = {}
 
     for cloth, type in clothes:
         clothDict[type] = clothDict.get(type,0) + 1
 
-    print(clothDict)
-    
-    answer = 1
-    for clothType in clothDict:
-        answer *= (clothDict[clothType] + 1)
+    clothDict = Counter(type for name , type in clothes)
 
-    
-    return answer - 1
+    answer = reduce(lambda x, y : x * (y + 1), clothDict.values(), 1) - 1
+
+    return answer
 
 #clothes = [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]
 clothes = [["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]
