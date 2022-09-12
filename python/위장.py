80b858e6a93dd96d2e3d00046efbd917d0f3079c
MoonDD99 <mjh991016@naver.com>
2022-09-12 23:03:22 +0900

    Programmers : 위장

--- python/위장.py
+++ python/위장.py
@@ -2,11 +2,6 @@ from collections import Counter
 from functools import reduce
 
 def solution(clothes):
-    clothDict = {}
-
-    for cloth, type in clothes:
-        clothDict[type] = clothDict.get(type,0) + 1
-
     clothDict = Counter(type for name , type in clothes)
 
     answer = reduce(lambda x, y : x * (y + 1), clothDict.values(), 1) - 1
