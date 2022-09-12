740612ab2c263833f06ca48b8e453eb322d53f47
MoonDD99 <mjh991016@naver.com>
2022-09-12 23:44:13 +0900

    Programmers : 가장 큰 수

---
+++ python/가장 큰 수.py
@@ -0,0 +1,19 @@
+def solution(numbers):
+    answer = ''
+    stringNumbers = []
+
+    for number in numbers:
+        stringNumbers.append(str(number))
+    
+    #stringNumbers.sort(reverse=True)
+    sortStringNumbers = sorted(stringNumbers, key=lambda x:x*4, reverse=True)
+    #sortNumbers = sorted(list(map(str, numbers)), key=lambda x: x*5, reverse=True)
+    for stringNumber in sortStringNumbers:
+        answer += stringNumber
+
+    return answer
+
+#numbers = [6, 10, 2]
+numbers = [3, 30, 34, 5, 9]
+answer = solution(numbers)
+print(answer)
