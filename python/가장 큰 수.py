348d59a7884c55a652cca609986d919cce94b5c5
MoonDD99 <mjh991016@naver.com>
2022-09-13 18:01:20 +0900

    Programmers : 가장 큰 수

--- python/가장 큰 수.py
+++ python/가장 큰 수.py
@@ -11,9 +11,10 @@ def solution(numbers):
     for stringNumber in sortStringNumbers:
         answer += stringNumber
 
-    return answer
+    return str(int(answer))
 
 #numbers = [6, 10, 2]
-numbers = [3, 30, 34, 5, 9]
+#numbers = [3, 30, 34, 5, 9]
+numbers = [0,0,0,0]
 answer = solution(numbers)
 print(answer)
