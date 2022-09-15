d28520017f1ebe2804254c7181b38f28caceaec4
MoonDD99 <mjh991016@naver.com>
2022-09-15 22:55:25 +0900

    Programmers : 소수 찾기

---
+++ python/소수 찾기.py
@@ -0,0 +1,29 @@
+from itertools import permutations
+
+def isPrimeNum(number):
+    for divisor in range(2, number, 1):
+        if number % divisor == 0:
+            return False
+        return True
+
+def solution(numbers):
+    primeNumSet = set()
+    numbersArray = list(numbers)
+
+    #가능한 모든 숫자 만들기 string-> int
+    for i in range(1,len(numbers)+1,1):
+        possiblePermutations = list(permutations(numbersArray,i))
+        for permutation in possiblePermutations:
+            number = int(''.join(permutation))
+            #소수인지 검사
+            if isPrimeNum(number):
+                #numberSet에 저장
+                primeNumSet.add(number)
+        
+    #primeNumSet 길이 return
+    return len(primeNumSet)
+
+#numbers = "17"
+numbers = "011"
+answer = solution(numbers)
+print(answer)
