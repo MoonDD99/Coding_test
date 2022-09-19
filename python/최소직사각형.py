a708052c1b5fa199bc0374c05eab5935e12a23f4
MoonDD99 <mjh991016@naver.com>
2022-09-19 22:57:23 +0900

    Programmers : 최소직사각형

--- python/최소직사각형.py
+++ python/최소직사각형.py
@@ -4,12 +4,14 @@ def solution(sizes):
         width = size[0]
         height = size[1]
         for compareSize in sizes:
-            if not(compareSize[0] == width and compareSize[1] == height) and ((compareSize[0] <= width and compareSize[1] <= height) or (compareSize[0] <= height and compareSize[1] <= width)) :
-                print(size)
-                result.update([width*height])
-        
+            if (compareSize[0] > width or compareSize[1] > height) or (compareSize[0] > height or compareSize[1] > width) :
+                continue
+        result.update([width*height])
+    print(result)
     return min(result)
 
 sizes=[[60, 50], [30, 70], [60, 30], [80, 40]]
-answer = solution(sizes)
-print(answer)
+sizes.sort()
+print(sizes)
+#answer = solution(sizes)
+#print(answer)
