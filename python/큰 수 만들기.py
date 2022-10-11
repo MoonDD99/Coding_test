d0c0ff1176f5209512541e1f3983996a94bd0ea7
MoonDD99 <mjh991016@naver.com>
2022-10-11 19:17:40 +0900

    프로그래머스 : 큰 수 만들기

---
+++ python/큰 수 만들기.py
@@ -0,0 +1,31 @@
+def failSolution(number, k):
+    result = ""
+    restLen = len(number) - k
+    maxIndex = 0
+    for _ in range(0,restLen) :
+        for index in range(maxIndex + 1, len(number) - restLen + 1):
+            if number[maxIndex] < number[index]:
+                maxIndex = index    
+        result += number[maxIndex]
+        restLen -= 1
+        maxIndex += 1
+
+    return result            
+def solution(number, k):
+    result = []
+    
+    for num in number:
+        while result and result[-1] < num and k > 0 :
+            result.pop()
+            k -= 1
+        result.append(num)
+    
+    return ''.join(result[:len(result) - k])
+number = "1924"
+k = 2
+number = "1231234"
+k = 3
+number ="4177252841"
+k = 4 
+result = solution(number, k)
+print(result)
