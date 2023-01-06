43b9d09fa16537d29968f8ec9034f51dcd952195
MoonDD99 <mjh991016@naver.com>
2023-01-06 15:39:56 +0900

    BackJoon : 2178

---
+++ python/2178.py
@@ -0,0 +1,40 @@
+from collections import deque
+N, M = map(int, input().split())
+
+graph = []
+
+for _ in range(N):
+  graph.append(list(map(int, input())))
+
+def solution(graph):
+    answer = 0
+    #상하좌우
+    dr = [-1,1,0,0]
+    dc = [0,0,-1,1]
+
+
+    def bfs(firstRow, firstCol):
+        que = deque()
+        que.append((firstRow, firstCol))
+
+        while que:
+            row, col = que.popleft()
+            if row == N-1 and col == M-1 :
+                return graph[N-1][M-1]
+            for index in range(4):
+                nextRow = row + dr[index]
+                nextCol = col + dc[index]
+
+                if nextRow < 0 or nextRow >= N or nextCol < 0 or nextCol >= M:
+                    continue
+                if graph[nextRow][nextCol] == 0:
+                    continue
+                if graph[nextRow][nextCol] == 1:
+                    graph[nextRow][nextCol] = graph[row][col] + 1
+                    que.append((nextRow, nextCol))
+
+                
+    answer = bfs(0,0)
+    print(answer)
+
+solution(graph)
