4350fd4892ad6a3a81abe2570c5afce7f85067c6
MoonDD99 <mjh991016@naver.com>
2023-01-19 09:23:17 +0900

    Programmers : 아이템 줍기

--- python/아이템 줍기.py
+++ python/아이템 줍기.py
@@ -7,19 +7,19 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
     graph = makeGraph(rectangle)
 
     que = deque()
-    que.append((characterY, characterX))
+    que.append((characterY*2, characterX*2))
 
     while que:
         y,x = que.popleft()
-        print("x : " + str(x) + " y : " +  str(y) + " distance : " + str(graph[y][x]))
-        if x == itemX and y == itemY:
-            return graph[y][x] - 1
+        #print("x : " + str(x) + " y : " +  str(y) + " distance : " + str(graph[y][x]))
+        if x == 2 * itemX and y == 2 * itemY:
+            return graph[y][x] // 2
 
         for index in range(4):
             nextX = x + dx[index]
             nextY = y + dy[index]
 
-            if nextX >= 0 and nextX <= 50 and nextY >=0 and nextY <= 50 and graph[nextY][nextX] == 1:
+            if graph[nextY][nextX] == 1: 
                 que.append((nextY,nextX))
                 graph[nextY][nextX] = graph[y][x] + 1
             
@@ -28,18 +28,18 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
     return answer
 
 def makeGraph(rectangles):
-    graph = [[-1] * 51 for _ in range(51)] # 빈곳 : -1 직사각형 내부 : 0 테두리 : 1
+    graph = [[-1] * 102 for _ in range(102)] # 빈곳 : -1 직사각형 내부 : 0 테두리 : 1
 
     for rectangle in rectangles:
-        lx, ly, rx, ry = rectangle #[좌x, 좌y, 우x, 우y]
+        lx, ly, rx, ry = map(lambda x : x*2,rectangle) #[좌x, 좌y, 우x, 우y]
         for row in range(ly, ry+1, 1):
             for col in range(lx, rx+1, 1):
-                if (graph[row][col] != 0) and ((row == ly and col <= rx) or (row == ry and col <= rx) or (row <= ry and col == lx) or (row <= ry and col == rx)): #not내부 and (밑변 or 윗변 or 좌변 or 우변)
+                if lx < col < rx and ly < row < ry:
                     #테두리 : 1
-                    graph[row][col] = 1
-                else:
-                    #직사각형 내부 : 0
                     graph[row][col] = 0
+                elif graph[row][col] != 0:
+                    #직사각형 내부 : 0
+                    graph[row][col] = 1
     return graph
 
 def checkGraph(rectangle):
@@ -49,11 +49,11 @@ def checkGraph(rectangle):
             if newGraph[row][col] == 1:
                 print((row,col))
 
-rectangle = [[1,1,7,4],[3,2,5,5],[4,3,6,9],[2,6,8,8]]
-characterX = 1
-characterY = 3	
-itemX = 7
-itemY = 8
+rectangle = [[1,1,8,4],[2,2,4,9],[3,6,9,8],[6,3,7,7]]
+characterX = 9
+characterY = 7	
+itemX = 6
+itemY = 1
 #checkGraph(rectangle)
 answer = solution(rectangle, characterX, characterY, itemX, itemY)
-#print(answer)
+print(answer)
