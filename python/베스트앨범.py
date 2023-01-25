b852a4811ebbe1afba71690baa41ad67edc24170
MoonDD99 <mjh991016@naver.com>
2023-01-25 17:38:27 +0900

    Programmers : 베스트앨범

--- python/베스트앨범.py
+++ python/베스트앨범.py
@@ -1,27 +1,20 @@
 from collections import defaultdict, deque
 def solution(genres, plays):
     answer = []
-    genreSong = {} #{genre : [(index,play)]}
-    genreTotalPlay = {} #{genre : totalPlay} {totalPlay : genre}
+    genreSong = defaultdict(list) #{genre : [(index,play)]}
+    genreTotalPlay = defaultdict(int) #{genre : totalPlay} {totalPlay : genre}
 
     for index in range(len(genres)):
         genre = genres[index]
         play = plays[index]
         
-        if genre not in genreSong:
-            genreSong[genre] = [(index, play)]
-        else:    
-            genreSong[genre].append((index, play))
-            
-        if genre not in genreTotalPlay:
-            genreTotalPlay[genre] = play    
-        else:
-            genreTotalPlay[genre] += play
+        genreSong[genre].append((index, play))
+        genreTotalPlay[genre] += play
 
     
     sortTotalPlay = sorted(genreTotalPlay.items(), key=lambda x:x[1], reverse=True)
     
-    for key, value in sortTotalPlay[:2]:
+    for key, value in sortTotalPlay:
         sortSong = sorted(genreSong[key],key=lambda x:x[1], reverse=True)
         for index, play in sortSong[:2]:
             answer.append(index)
@@ -32,7 +25,7 @@ def solution(genres, plays):
     return answer
 
 
-genres = ["classic", "pop", "classic", "classic", "pop"]
-plays = [500, 600, 150, 800, 2500]	
+genres = ["a", "b", "b", "c", "c"] #["classic", "pop", "classic", "pop", "classic", "classic"]#["classic", "pop", "classic", "classic", "pop"] #["classic", "pop", "classic", "classic"]
+plays = [5, 5, 40, 5, 5]#[1950, 600, 500, 2500, 500, 150] #[500, 600, 150, 800, 2500]	#[800, 600, 150, 800]
 answer = solution(genres, plays)
 print(answer)
