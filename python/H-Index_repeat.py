cf701b24a938521d41314d10cef462c7dbbff48e
MoonDD99 <mjh991016@naver.com>
2023-01-30 10:55:39 +0900

    Programmers : H-Index

--- python/H-Index_repeat.py
+++ python/H-Index_repeat.py
@@ -6,7 +6,14 @@ def solution(citations):
             return lenCitations - h
     return 0
 
-
-citations = [3, 0, 6, 1, 5]
-answer = solution(citations)
+def failSolution(citations):
+    sortCitations = sorted(citations, reverse=True)
+    for index in range(len(sortCitations)):
+        if (index + 1) >= sortCitations[index]:
+            return index + 1
+    return len(sortCitations)
+#citations = [3, 0, 6, 1, 5]
+#citations = [10000, 9999, 9998, 9997, 9996]
+citations = [10,10,10,10]
+answer = solution1(citations)
 print(answer)
