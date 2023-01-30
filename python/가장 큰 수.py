2f094caf71ddd56db0098c46d4af792c1a837334
MoonDD99 <mjh991016@naver.com>
2023-01-30 10:22:08 +0900

    Programmers : 가장 큰 수

--- python/가장 큰 수.py
+++ python/가장 큰 수.py
@@ -4,17 +4,30 @@ def solution(numbers):
 
     for number in numbers:
         stringNumbers.append(str(number))
-    
-    #stringNumbers.sort(reverse=True)
-    sortStringNumbers = sorted(stringNumbers, key=lambda x:x*4, reverse=True)
-    #sortNumbers = sorted(list(map(str, numbers)), key=lambda x: x*5, reverse=True)
+
+    sortStringNumbers = sorted(stringNumbers, key=lambda x:x*3, reverse=True)
+
     for stringNumber in sortStringNumbers:
         answer += stringNumber
 
     return str(int(answer))
 
-#numbers = [6, 10, 2]
+def failSolution(numbers):
+    answer = ''
+    stringNumbers = []
+
+    for number in numbers:
+        stringNumbers.append(str(number))
+
+    sortStringNumbers = sorted(stringNumbers, key=lambda x:x*3, reverse=True)
+
+    for stringNumber in sortStringNumbers:
+        answer += str(int(stringNumber)) #여기서 int로 바꾸면 [0,0,0,0]에서 answer에서 "0000"으로 출력된다
+
+    return answer 
+
+numbers = [6, 10, 2]
 #numbers = [3, 30, 34, 5, 9]
-numbers = [0,0,0,0]
+#numbers = [0,0,0,0]
 answer = solution(numbers)
 print(answer)
