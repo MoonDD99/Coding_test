3f0e639b79e204484ce22df97dc988605dcc6df7
MoonDD99 <mjh991016@naver.com>
2023-02-07 17:58:34 +0900

    Programmers : 방문 길이

--- python/방문 길이.py
+++ python/방문 길이.py
@@ -18,3 +18,25 @@ def solution(dirs):
             current_y = next_y
 
     return len(way) // 2
+
+from collections import defaultdict
+def solution1(dirs):
+    dx = {"U": 0, "D":0, "R":1, "L":-1} #col
+    dy = {"U": 1, "D":-1, "R":0, "L":0} #row
+    visited = defaultdict(list)
+    answer = 0
+    
+    x, y = 0,0
+    for dir in dirs:
+        nextX, nextY = x + dx[dir], y + dy[dir]
+        if nextX <= 5 and nextY <= 5 and nextX >= -5 and nextY >= -5:
+            if (nextX,nextY) not in visited[(x,y)] :
+                visited[(x,y)].append((nextX, nextY))
+                visited[(nextX,nextY)].append((x, y))
+                answer += 1
+            x, y = nextX, nextY
+
+    return answer
+
+dirs = "ULURRDLLU"	
+answer = solution(dirs)
