7596f871b7dd40fa168b0f97cf768895b4915377
MoonDD99 <mjh991016@naver.com>
2023-02-14 17:46:47 +0900

    LeetCode : 347.TopKFrequentElements

--- python/347TopKFrequentElements.py
+++ python/347TopKFrequentElements.py
@@ -1,4 +1,5 @@
-from collections import defaultdict
+from collections import defaultdict,Counter
+import heapq
 class Solution:
     def topKFrequent(nums, k):
         numCount = defaultdict(int)
@@ -13,8 +14,16 @@ class Solution:
             answer.append(num)
 
         return answer
+    def topKFrequent1(nums, k):
+        if k == len(nums):
+            return nums
+        
+        numCount = Counter(nums)
+        print(numCount.get)
+        return heapq.nlargest(k,numCount.keys(), key=numCount.get)
 
-nums =[1]
-k=1
-answer = Solution.topKFrequent(nums,k)
+
+nums =[1,1,1,1,2,2,2,3,3]
+k=2
+answer = Solution.topKFrequent1(nums,k)
 print(answer)
