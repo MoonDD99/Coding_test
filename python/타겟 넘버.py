1298547275abd1f8ac8abc42c9aefc0cfeabe3cc
MoonDD99 <mjh991016@naver.com>
2023-04-12 16:27:06 +0900

    Programmers : 타겟 넘버

--- python/타겟 넘버.py
+++ python/타겟 넘버.py
@@ -12,3 +12,20 @@ def dfs(numbers, target, depth):
         numbers[depth] *= -1
         answer += dfs(numbers, target, depth+1)
         return answer
+
+#230412
+def solution(numbers, target):
+    answer = dfs(numbers, target, 0)
+    return answer
+def dfs(numbers, target, depth):
+    answer = 0
+    if depth == len(numbers):
+        if sum(numbers) == target:
+            return 1
+        return 0
+    else:
+        answer += dfs(numbers, target, depth+1)
+        numbers[depth] *= -1
+        answer += dfs(numbers, target, depth+1)
+        return answer
+        
